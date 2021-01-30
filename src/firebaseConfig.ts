5515f8984a01994c5041330cb53a69bfdcae57e4
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2021-01-31 02:08:52 +0530

    Added databaseUrl for Firestore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,12 +3,13 @@ import "firebase/auth";
 
 const app = firebase.initializeApp({
     apiKey: "AIzaSyDqxvky9QplU3Wso74MRcI3W46vJCu637Q",
-    authDomain: "coffee-chat-90f5d.firebaseapp.com",
-    projectId: "coffee-chat-90f5d",
-    storageBucket: "coffee-chat-90f5d.appspot.com",
-    messagingSenderId: "352878256726",
-    appId: "1:352878256726:web:4fa6eb3154e66ba32351f7",
-    measurementId: "G-DN0EDHGE97"
+  authDomain: "coffee-chat-90f5d.firebaseapp.com",
+  databaseURL: "https://coffee-chat-90f5d.firebaseio.com",
+  projectId: "coffee-chat-90f5d",
+  storageBucket: "coffee-chat-90f5d.appspot.com",
+  messagingSenderId: "352878256726",
+  appId: "1:352878256726:web:4fa6eb3154e66ba32351f7",
+  measurementId: "G-DN0EDHGE97"
   });
 
   export default app;
