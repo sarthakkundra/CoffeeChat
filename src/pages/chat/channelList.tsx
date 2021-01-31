04dac0aa28df9193ff1125993b344eed1057769b
Seyoung Park <park_seyoung@outlook.com>
2021-01-31 19:10:38 +0800

    Add chat UI

---
+++ src/pages/chat/channelList.tsx
@@ -0,0 +1,14 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+
+export default function ChannelList() {
+  return (
+    <Flex flex={1} maxH="100%" minH="0" direction="column" overflowY="auto">
+      <Text>Channel</Text>
+      <Text>Channel</Text>
+      <Text>Channel</Text>
+      <Text>Channel</Text>
+      <Text>Channel</Text>
+    </Flex>
+  );
+}
